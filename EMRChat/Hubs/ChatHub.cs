4bd9bc2540dfa8f5ca6e9015ea3972f8e95b4b7e
ldmsgmkumar.santha <ldmsgmkumar.santha@kjsystems.in>
2020-07-02 19:36:02 +0530

    New Code Added

--- EMRChat/Hubs/ChatHub.cs
+++ EMRChat/Hubs/ChatHub.cs
@@ -11,28 +11,17 @@ using System.Security.Claims;
 using System.Collections.Concurrent;
 
 using System.IO;
+using System.Collections.Generic;
 
 namespace EMRChat.Hubs
 {
-
-    public static class Users
-    {
-
-    }
-
-
-
     public class MessageHandler
     {
 
     }
 
-
-
     public class ChatHub : Hub
     {
-
-        
         /// Step 1: Connect
         ///     Save ConnectionId and 
         ///     UserId filter
@@ -45,48 +34,26 @@ namespace EMRChat.Hubs
         ///     Show Online -> Connection Id no need
         ///     When all connections are closed remove the respective user
         /// 
-        ConcurrentDictionary<int, ConcurrentDictionary<string, User>> connectedUsers = new ConcurrentDictionary<int, ConcurrentDictionary<string, User>>();
 
-        private static object userLock = new object();
+        private readonly UserHandler _userHandler = new UserHandler();          
+       
 
         public async Task OnTextMessage(string chatInfo)
         {
             var deserializedChatInfo = JsonSerializer.Deserialize<ChatInfo>(chatInfo);
-            await Clients.User(deserializedChatInfo.ToUser.ToString()).SendAsync("OnTextMessage", chatInfo);
+
+            await Clients.User(deserializedChatInfo.ToUser.UserIdentifier).SendAsync("OnTextMessage", chatInfo);
         }
 
 
         public async Task OnUserConnected(string userInfo)
         {
+           // var connectionUser = JsonConvert.DeserializeObject<User>(userInfo);
             var connectionUser = JsonSerializer.Deserialize<User>(userInfo);
-            connectionUser.ConnectionIds.Add(Context.ConnectionId);
-            var practiceId = int.Parse(Context.UserIdentifier.Split("_").First());
 
+            _userHandler.AddConnectedUser(connectionUser, Context);
 
-            if (connectedUsers.TryGetValue(practiceId, out ConcurrentDictionary<string, User> practiceUsers))
-            {
-                if (practiceUsers.TryGetValue(Context.UserIdentifier, out User connectedUser))
-                {
-                    lock (userLock)
-                    {
-                        connectedUser.ConnectionIds.Add(Context.ConnectionId);
-                    }
-                }
-                else
-                {
-                    practiceUsers.TryAdd(Context.UserIdentifier, connectedUser);
-                }
-            }
-            else
-            {
-                practiceUsers = new ConcurrentDictionary<string, User>() { [Context.UserIdentifier] = connectionUser };
-                connectedUsers.TryAdd(practiceId, practiceUsers);
-            }
-
-            var usersToNotify = connectedUsers[practiceId]
-                .Where(x => x.Key != Context.UserIdentifier)
-                .Select(x => x.Key)
-                .ToList();
+            var usersToNotify = _userHandler.UserIdsToNotify(connectionUser).ToList();
 
             await Clients.Users(usersToNotify).SendAsync("OnUserConnected", connectionUser);
         }
@@ -99,21 +66,17 @@ namespace EMRChat.Hubs
 
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            var practiceId = int.Parse(Context.UserIdentifier.Split("_").First());
-
-            var disconnectedUser = connectedUsers[practiceId][Context.UserIdentifier];
-
-            lock (userLock)
-            {
-                disconnectedUser.ConnectionIds.Remove(Context.ConnectionId);
-
-                if (disconnectedUser.ConnectionIds.Count() == 0)
-                {
-                    connectedUsers[practiceId].TryRemove(Context.UserIdentifier, out _);
-                }
-            }
-
+            _userHandler.RemoveUserConnection(Context);
             return base.OnDisconnectedAsync(exception);
         }
+        
+        public IEnumerable<User> UsersList(string userInfo)
+        {
+            var fromUserInfo = JsonSerializer.Deserialize<User>(userInfo);
+            
+           // await Clients.Caller.SendAsync("UsersList",_userHandler.UsersList(fromUserInfo));
+            return _userHandler.UsersList(fromUserInfo);
+        }
+
     }
 }
